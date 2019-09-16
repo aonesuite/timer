7b15dd6091da9e41419ed48fe310207c9b5a6292
Miclle Zheng <miclle.zheng@gmail.com>
2019-09-17 01:43:16 +0800

    Add 360 degree encoder switch

--- timer.ino
+++ timer.ino
@@ -11,37 +11,72 @@ unsigned char second = 0;
 unsigned char minute = 0;
 unsigned char hour = 0;
 
-#define CLK 3 //pins definitions for TM1637 and can be changed to other ports
+// 时钟引脚， Arduino Nano 只有 2，3 引脚支持 attachInterrupt
+#define CLK 3
 #define DIO 2
 TM1637 tm1637(CLK, DIO);
 
-const int resetSwitchPin = 4; // 重置开关按键
+const int resetButtonPin = 4; // 重置开关按键
 const int buzzPin = 5;        // 蜂鸣器
-const int VibPin = 6;         // 震动模块
+const int vibratorPin = 6;    // 震动模块
+
+// 旋钮
+const int encoderButtonPin = 7; // 旋钮开关
+const int encoderPinA = 8;      //
+const int encoderPinB = 9;      //
+
+volatile int lastEncoded = 0;
+volatile long encoderValue = 0;
+
+long lastencoderValue = 0;
+
+int lastMSB = 0;
+int lastLSB = 0;
 
 void setup()
 {
   Serial.begin(9600);
   Serial.println("TIMER SETUP");
 
-  pinMode(resetSwitchPin, INPUT); // 定义重置开关按键引脚为输入引脚
+  pinMode(resetButtonPin, INPUT); // 定义重置开关按键引脚为输入引脚
   pinMode(buzzPin, OUTPUT);       // 定义蜂鸣器引脚为输出引脚
-  pinMode(VibPin, OUTPUT);        // 定义震动模块引脚为输出引脚
+  pinMode(vibratorPin, OUTPUT);   // 定义震动模块引脚为输出引脚
 
+  pinMode(encoderPinA, INPUT);
+  pinMode(encoderPinB, INPUT);
+  pinMode(encoderButtonPin, INPUT);
+
+  digitalWrite(encoderPinA, HIGH); // turn pullup resistor on
+  digitalWrite(encoderPinB, HIGH); // turn pullup resistor on
+
+  // 时钟初始化，定义计时器中断服务程序
   tm1637.set();
   tm1637.init();
-  Timer1.initialize(500000);         //timing for 500ms
-  Timer1.attachInterrupt(TimingISR); //declare the interrupt serve routine:TimingISR
+  Timer1.initialize(500000);         // timing for 500ms
+  Timer1.attachInterrupt(TimingISR); // declare the interrupt serve routine:TimingISR
 }
 
 void loop()
 {
-  int val = digitalRead(resetSwitchPin); //读取输入值
+  if (IsEncoderButtonPushDown())
+  {
+    Serial.println("you push button down!!!");
+    // TODO: 切换显示模式
+  }
+
+  // 在 loop 函数中代替 attachInterrupt 来监听更新 Encoder
+  UpdateEncoder();
+
+  // 输出 encoder value
+  // TODO: 用于设置时间或报警时长
+  Serial.println(ReadEncoderValue());
+
+  // 重置按钮按下时，数值归零
+  int val = digitalRead(resetButtonPin); // 读取输入值
   if (val == HIGH)                       // 检查输入是否为高，这里高为按下
   {
-    Serial.println("resetSwitchPin HIGH");
-    digitalWrite(buzzPin, HIGH); // 开启蜂鸣器
-    digitalWrite(VibPin, HIGH);  // 开启震动器
+    Serial.println("resetButtonPin HIGH");
+    Alert(true, true);
 
     ClockPoint = 1;
     halfsecond = 0;
@@ -52,23 +87,18 @@ void loop()
   }
   else
   {
-    digitalWrite(buzzPin, LOW); // 关闭蜂鸣器
-    digitalWrite(VibPin, LOW);  // 关闭震动器
+    Alert(false, false);
   }
 
-  Serial.print("second: ");
-  Serial.println(second);
-
+  // 每分钟报警一次
   if (second == 59)
   {
-    digitalWrite(buzzPin, HIGH);
-    digitalWrite(VibPin, HIGH);
+    Alert(true, true);
     delay(50);
   }
   else
   {
-    digitalWrite(buzzPin, LOW);
-    digitalWrite(VibPin, LOW);
+    Alert(false, false);
   }
 
   if (Update == ON)
@@ -78,6 +108,7 @@ void loop()
   }
 }
 
+// TimingISR 计时器中断服务程序
 void TimingISR()
 {
   halfsecond++;
@@ -105,6 +136,7 @@ void TimingISR()
   ClockPoint = (~ClockPoint) & 0x01;
 }
 
+// TimeUpdate 更新数码管显示时间
 void TimeUpdate(void)
 {
   tm1637.point(ClockPoint ? POINT_ON : POINT_OFF);
@@ -114,11 +146,46 @@ void TimeUpdate(void)
   TimeDisp[2] = second / 10;
   TimeDisp[3] = second % 10;
   Update = OFF;
+}
 
-  Serial.print("Time Display: ");
-  Serial.print(TimeDisp[0]);
-  Serial.print(TimeDisp[1]);
-  Serial.print(":");
-  Serial.print(TimeDisp[2]);
-  Serial.println(TimeDisp[3]);
+// UpdateEncoder 更新 360 度编码器开关数值
+void UpdateEncoder()
+{
+  int MSB = digitalRead(encoderPinA); // MSB = most significant bit
+  int LSB = digitalRead(encoderPinB); // LSB = least significant bit
+
+  int encoded = (MSB << 1) | LSB;         // converting the 2 pin value to single number
+  int sum = (lastEncoded << 2) | encoded; // adding it to the previous encoded value
+
+  if (sum == 0b1101 || sum == 0b0100 || sum == 0b0010 || sum == 0b1011)
+    encoderValue++;
+  if (sum == 0b1110 || sum == 0b0111 || sum == 0b0001 || sum == 0b1000)
+    encoderValue--;
+
+  lastEncoded = encoded; // store this value for next time
+}
+
+// ReadEncoderValue 读取 360 度编码器数值
+long ReadEncoderValue(void)
+{
+  return encoderValue / 4;
+}
+
+// IsEncoderButtonPushDown 360 度编码器开关是否按下
+boolean IsEncoderButtonPushDown(void)
+{
+  if (!digitalRead(encoderButtonPin))
+  {
+    delay(5);
+    if (!digitalRead(encoderButtonPin))
+      return true;
+  }
+  return false;
+}
+
+// Alert 蜂鸣和震动
+void Alert(boolean buzz, boolean vibrator)
+{
+  digitalWrite(buzzPin, buzz ? HIGH : LOW);
+  digitalWrite(vibratorPin, vibrator ? HIGH : LOW);
 }
